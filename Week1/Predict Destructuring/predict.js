538fa69b6c75b51585bfbb7e14f9919647d43802
Ethan-Guenther <ethan.guenther98@gmail.com>
2022-06-07 11:48:56 -0500

    commit

---
+++ Week1/Predict Destructuring/predict.js
@@ -0,0 +1,72 @@
+// const cars = ['Tesla', 'Mercedes', 'Honda']
+// const [ randomCar ] = cars
+// const [ ,otherRandomCar ] = cars
+// //Predict the output
+// console.log(randomCar)
+// console.log(otherRandomCar)
+
+//Predicted output Problem 1
+//Tesla
+//Mercedes
+
+// const employee = {
+//     name: 'Elon',
+//     age: 47,
+//     company: 'Tesla'
+// }
+// const { name: otherName } = employee;
+// //Predict the output
+// console.log(name);
+// console.log(otherName);
+
+//Predicted output Problem 2
+//Reference Error name is not defined
+
+
+// const person = {
+//     name: 'Phil Smith',
+//     age: 47,
+//     height: '6 feet'
+// }
+// const password = '12345';
+// const { password: hashedPassword } = person;  
+// //Predict the output
+// console.log(password);
+// console.log(hashedPassword);
+
+//Predicted output Problem 3
+//12345
+//undefined
+
+// const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+// const [,first] = numbers;
+// const [,,,second] = numbers;
+// const [,,,,,,,,third] = numbers;
+// //Predict the output
+// console.log(first == second);
+// console.log(first == third);
+
+//Predicted output Problem 4
+//false
+//true
+
+// const lastTest = {
+//     key: 'value',
+//     secondKey: [1, 5, 1, 8, 3, 3]
+// }
+// const { key } = lastTest;
+// const { secondKey } = lastTest;
+// const [ ,willThisWork] = secondKey;
+// //Predict the output
+// console.log(key);
+// console.log(secondKey);
+// console.log(secondKey[0]);
+// console.log(willThisWork);
+
+//Predicted output Problem 5
+//value
+//1,5,1,8,3,3
+//1
+//5
+
+
