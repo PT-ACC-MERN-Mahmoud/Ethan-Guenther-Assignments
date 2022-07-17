2f92e5e9f39c6a23f7465ded80a92dccdcf606e1
Ethan-Guenther <ethan.guenther98@gmail.com>
2022-07-16 19:01:00 -0500

    commit

--- Week6/ProductManager/client/src/components/AllProducts.js
+++ Week6/ProductManager/client/src/components/AllProducts.js
@@ -1,10 +1,11 @@
 import React, {useEffect} from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 const AllProducts = (props) => {
 
     const {productList, setProductList} = props;
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get('http://localhost:8000/api/products')
@@ -15,16 +16,36 @@ const AllProducts = (props) => {
             })
             .catch((err) => console.log("Error grabbing all product list", err))
     }, [])
+        
+    const deleteProduct = (product_id) => {
+        axios.delete(`http://localhost:8000/api/products/${product_id}`)
+        .then((res) => {
+            console.log(res.data);
+
+            const updatedList = productList.filter((product, index) => product._id != product_id)
+
+            setProductList(updatedList);
+
+        })
+
+        .catch((err) => console.log(err));
+    };
 
     return (
         <div>
             <h3>Products:</h3>
-
             {
                 productList.map((product, index) => (
                     <div key={product._id}>
                         <Link to={`/products/${product._id}`}>{product.title}</Link>
+                        
+                        <button onClick={()=> navigate(`/products/edit/${product._id}`)}>
+                            Edit Product</button>
+
+                        <button onClick={() => deleteProduct(product._id)}> 
+                        DELETE</button>
                     </div>
+                    
                 ))
 
                 }
--- Week6/ProductManager/client/src/components/ProductById.js
+++ Week6/ProductManager/client/src/components/ProductById.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import {useParams} from 'react-router-dom';
+import {useParams, useNavigate} from 'react-router-dom';
 const ProductById = (props) => {
 
+    const navigate = useNavigate();
     const [productById, setProductById] = useState({});
     const {id} = useParams();
     useEffect(() => {
@@ -17,11 +18,24 @@ const ProductById = (props) => {
 
     }, [])
 
+
+    const handleDelete = () => {
+        axios.delete(`http://localhost:8000/api/products/${id}`)
+        .then((res) => {
+            console.log(res);
+            console.log(res.data);
+            
+            navigate('/');
+        })
+        .catch((err) => console.log("There was an error deleting a product", err));
+    }
+
     return (
         <div>
             <h3>{productById.title}</h3>
             <p>Price: {productById.price}</p>
             <p>Description: {productById.description}</p>
+            <button onClick={handleDelete}>Delete Product</button>
         </div>
     )
 }
